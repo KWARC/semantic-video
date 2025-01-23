8ab48e0d3f71c288bb9569124934d296dfc54e47
Abhishek Kumar <abhishek2021005@gmail.com>
2025-01-23 12:07:39 +0530

    minor change
    Signed-off-by: Abhishek Kumar <abhishek2021005@gmail.com>

--- scripts/slide_matcher.py
+++ scripts/slide_matcher.py
@@ -5,31 +5,31 @@ from rapidfuzz import fuzz, process
 
 load_dotenv(".env.local")
 
-slides_file_path = os.getenv("SLIDES_FILE_PATH")
-results_file_path = os.getenv("RESULTS_FILE_PATH")
-output_file_path = os.getenv("OUTPUT_FILE_PATH")
+SLIDES_FILE_PATH = os.getenv("SLIDES_FILE_PATH")
+OCR_CACHE_FILE_PATH = os.getenv("OCR_CACHE_FILE_PATH")
+PROCESSED_OUTPUT_FILE_PATH = os.getenv("PROCESSED_OUTPUT_FILE_PATH")
 
-if not all([slides_file_path, results_file_path, output_file_path]):
+if not all([SLIDES_FILE_PATH, OCR_CACHE_FILE_PATH, PROCESSED_OUTPUT_FILE_PATH]):
     raise EnvironmentError("Missing required environment variables. Check .env.local.")
 
-with open(slides_file_path, "r", encoding="utf-8") as slides_file:
+with open(SLIDES_FILE_PATH, "r", encoding="utf-8") as slides_file:
     slides = json.load(slides_file)
 
-with open(results_file_path, "r", encoding="utf-8") as results_file:
+with open(OCR_CACHE_FILE_PATH, "r", encoding="utf-8") as results_file:
     results = json.load(results_file)
 
 text_data = []
-for video_id, video_info in results.items():
-    url = video_info["url"]
-    for key, text_entry in video_info["extracted_text"].items():
-        text_data.append(
-            {
-                "start_time": text_entry["start_time"],
-                "end_time": text_entry["end_time"],
-                "text_value": text_entry["text_value"],
-                "url": url,
-            }
-        )
+for video_id, video_data in results.items():
+    if "extracted_text" in video_data:  # Check if extracted_text exists
+        for timestamp, text_entry in video_data["extracted_text"].items():
+            text_data.append(
+                {
+                    "start_time": text_entry["start_time"],
+                    "end_time": text_entry["end_time"],
+                    "text_value": text_entry["text_value"],
+                    "video_id": video_id,
+                }
+            )
 
 for slide in slides:
     slide_text = slide["slideContent"]
@@ -48,12 +48,11 @@ for slide in slides:
                 entry for entry in text_data if entry["text_value"] == matched_text
             )
 
-            slide["video_name"] = matched_entry["video_name"]
             slide["start_time"] = matched_entry["start_time"]
             slide["end_time"] = matched_entry["end_time"]
-            slide["url"] = matched_entry["url"]
+            slide["video_id"] = matched_entry["video_id"]
 
-with open(output_file_path, "w", encoding="utf-8") as output_file:
+with open(PROCESSED_OUTPUT_FILE_PATH, "w", encoding="utf-8") as output_file:
     json.dump(slides, output_file, indent=4, ensure_ascii=False)
 
 print("Slides updated with timestamps and URLs successfully!")
