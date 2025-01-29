9763e1a613b374f0d79b1c1f2927f2407015e204
Kundan <kundanims2020@gmail.com>
2025-01-29 18:14:10 +0530

    Update run_pipeline.sh

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -8,8 +8,9 @@ VENV_DIR="$PROJECT_DIR/venv"
 
 # Set the full path to the Python script
 SLIDE_FETCHER_SCRIPT="$PROJECT_DIR/scripts/slide_fetcher.py"
-SLIDE_MATCHER_SCRIPT="$PROJECT_DIR/scripts/slide_matcher.py"
 VIDEO_TEXT_EXTRACTOR_SCRIPT="$PROJECT_DIR/scripts/video_text_extractor.py"
+SLIDE_MATCHER_SCRIPT="$PROJECT_DIR/scripts/slide_matcher.py"
+
 
 # Change to the project directory
 cd "$PROJECT_DIR" || exit
@@ -18,7 +19,7 @@ cd "$PROJECT_DIR" || exit
 source "$VENV_DIR/bin/activate"
 
 "$VENV_DIR/bin/python" "$SLIDE_FETCHER_SCRIPT" && echo "$SLIDE_FETCHER_SCRIPT executed successfully!" || { echo "Error executing $SLIDE_FETCHER_SCRIPT"; exit 1; }
-"$VENV_DIR/bin/python" "$SLIDE_MATCHER_SCRIPT" && echo "$SLIDE_MATCHER_SCRIPT executed successfully!" || { echo "Error executing $SLIDE_MATCHER_SCRIPT"; exit 1; }
 "$VENV_DIR/bin/python" "$VIDEO_TEXT_EXTRACTOR_SCRIPT" && echo "$VIDEO_TEXT_EXTRACTOR_SCRIPT executed successfully!" || { echo "Error executing $VIDEO_TEXT_EXTRACTOR_SCRIPT"; exit 1; }
+"$VENV_DIR/bin/python" "$SLIDE_MATCHER_SCRIPT" && echo "$SLIDE_MATCHER_SCRIPT executed successfully!" || { echo "Error executing $SLIDE_MATCHER_SCRIPT"; exit 1; }
 
 echo "All scripts ran successfully!"
